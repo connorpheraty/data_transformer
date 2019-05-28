48f681d90dde4c91b96e4867d10a700be477effd
Connor <connorpheraty@gmail.com>
2019-05-28 14:10:55 -0500

    Adding initial POC package module

---
+++ lambdatas_connorpheraty/__init__.py
@@ -0,0 +1,13 @@
+#!/user/bin/env python
+"""
+lambdatas - a collection of Data Science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+from . import example_module 
+
+Y = example_module.increment(example_module.x) 
+
+TEST = pd.DataFrame(np.ones(10))
+
