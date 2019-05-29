fc3fc5791ab0f02f2c3c43233e70edc92e312d14
Connor <connorpheraty@gmail.com>
2019-05-29 09:21:59 -0500

    Modified code review script and fixed cardinality function

--- lambdatas_connorpheraty/Data_Transformers.py
+++ lambdatas_connorpheraty/Data_Transformers.py
@@ -8,31 +8,28 @@ class DataTransformer:
         self.df = df
     
     def root_squared(self, col):
+      '''Function that squares and roots all the values in a column.'''
         num = self[col] ** 2
         num = num ** .5
-        print(num)
+        return(num)
 
-    def print_head(self):
-        head = self.head()
-        print(head)
-
-    def cardinality(self):
+    def cardinality(self, thresh=10):
+      '''Function for machine learning preprocessing that bins columns into
+       high and low cardinality lists'''
         import pandas as pd
+
         high_card = []
         low_card = []
 
+        unique_df = self.nunique()
         unique_df = pd.DataFrame(self.nunique())
         unique_df = unique_df.reset_index()
-        #unique_df = unique_df.rename({'index':'col', })
-        print(unique_df)
 
-        for ind, num in unique_df:
-            for i in num:
-                print(i)    
-                if i > 10:
-                    high_card.append(i)
-                else:
-                    low_card.append(i)
+        for col, num in unique_df.values:
+            if num > thresh:
+                high_card.append(col)
+            else:
+                low_card.append(col)
           
         print("The High Cardinality columns are: ", high_card, "\n",
              "The Low Cardinality columns are: ", low_card)
